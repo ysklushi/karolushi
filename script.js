83289a68ef5e01e2572b5248ff2bbc74cdf29914
ysklushi <ysklushi@gmail.com>
2025-07-08 12:36:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+    });
+}
+
+// 確保 CSS 動畫只在需要時執行
+const style = document.createElement('style');
+style.innerHTML = `
+@keyframes navLinkFade {
+    from {
+        opacity: 0;
+        transform: translateX(50px);
+    }
+    to {
+        opacity: 1;
+        transform: translateX(0px);
+    }
+}
+`;
+document.head.appendChild(style);
+
+
+navSlide();
